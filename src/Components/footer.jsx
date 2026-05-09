a3a8eb673a509885a7db0d4b7bff5a00a4c2c5fe
Mmasi Sylvester <aniagolummasi@gmail.com>
2026-05-09 14:27:56 -0700

    added footer styling

---
+++ src/Components/footer.jsx
@@ -0,0 +1,34 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-top">
+        <div>
+          <h3>About</h3>
+
+          <p>
+            Group 20 built this planetary data project with React,
+            Fetch API, responsive CSS, and collaborative Git workflows.
+          </p>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="footer-bottom">
+        <div>
+          <p>Copyright 2026 Designed by Amaka & Ifeoma A.</p>
+          <p>
+            Built by <a href="https://github.com/Marvinpaul1/Group20-Capstone-Project.git" target="_blank" rel="noreferrer">Group 20</a>.
+            All rights reserved.
+          </p>
+        </div>
+
+        <a href="https://tsacademyonline.com/" target="_blank" rel="noreferrer">
+          TSAcademy
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
