95925eae2be1fb443524a02b5c6f843fa126b80a
anselaehi <anselaedewede@gmail.com>
2026-05-09 21:04:40 +0100

    I created and styled this planet tabular data in refernce to the template

---
+++ src/table.jsx
@@ -0,0 +1,93 @@
+import './table.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Table Component</h1>
+      {/* You can add your table component here */}
+      <table className="planet-table">
+        <thead>
+          <tr>
+            <th colSpan={2}></th>
+            <th>Name</th>
+            <th>Mass (10 24 kg)</th>
+            <th>Diameter (km)</th>
+            <th>Density (kg/m³)</th>
+            <th>Gravity (m/s²)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colSpan={2} rowSpan={4}>Terrestial Planets</td>
+            <td>Mercury</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td>Venus</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td>Earth</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td>Mars</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td rowSpan={4}>Jovian Planets</td>
+            <td rowSpan={2}>Gas Giants</td>
+            <td>Jupiter</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td>Saturn</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td rowSpan={2}>Ice Giants</td>
+            <td>Uranus</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Neptune</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Dwarf Planets</td>
+            <td>Pluto</td>
+            <td>0.330</td>
+            <td>4,878</td>
+            <td>5427</td>
+            <td>3.7</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
